46cc8e4a45fedde158aac1dd4c90727cc53a561a
Elishaya13 <a.lartillier@gmail.com>
2023-08-25 14:05:25 +0200

    Added relative fontsize, add PhotographerCard content

--- scripts/templates/PhotographerCard.js
+++ scripts/templates/PhotographerCard.js
@@ -8,11 +8,16 @@ class PhotographerCard {
 
     const photographerCard = `
     
-      <img
+      <img          
           alt="${this._photographer.name}"
           src="${this._photographer.portrait}"
+        
       />
+    
       <h2>${this._photographer.name}</h2>
+      <h3>${this._photographer.city}, ${this._photographer.country}</h3>
+      <p>${this._photographer.tagline}</p>
+      <span>${this._photographer.price}€/jour</span>
    
     `;
     $wrapper.innerHTML = photographerCard;

036119243246384b8ba692506a9d439552fdd160
Elishaya13 <a.lartillier@gmail.com>
2023-09-11 08:44:48 +0200

    Enhance SortBox class with comments and remove unused code

--- scripts/templates/SortBox.js
+++ scripts/templates/SortBox.js
@@ -5,36 +5,90 @@ class SortBox {
   }
 
   render() {
-    const $sortBox = `
-   
+    this.$wrapper.innerHTML = this.getSortBoxHTML();
+    this.setupCustomSelect();
+    return this.$wrapper;
+  }
+
+  getSortBoxHTML() {
+    return ` 
         <h4> Trier par </h4>
         <div class="custom_select">
             <select class="focusable_select" tabindex="0">
-                <option value="0">Popularité</option>
+                <option value="0">Popularité</option>                
                 <option value="1">Date</option>
                 <option value="2">Titre</option>
             </select>
-            <div class="select_selected">Popularité</div>
-            <div class="select_items">
-                <div>Popularité</div>
-                <div>Date</div>
-                <div>Titre</div>
+            <div class="selected_container">
+              <div class="select_selected">           
+              </div>
+              <span class="fa-solid fa-chevron-down"></span>
+            </div>            
+            <div class="select_items select_hide">
+              <hr class="divider">
+              <div>Popularité</div>
+              <hr class="divider">
+              <div>Date</div>
+              <hr class="divider">
+              <div>Titre</div>            
             </div>
-    </div>        
+        </div>        
     `;
-    this.$wrapper.innerHTML = $sortBox;
-    this.setupCustomSelect();
-    return this.$wrapper;
   }
   setupCustomSelect() {
     const select = this.$wrapper.querySelector(".custom_select select");
     const selectedDiv = this.$wrapper.querySelector(".select_selected");
     const selectItems = this.$wrapper.querySelector(".select_items");
     const options = this.$wrapper.querySelectorAll(".select_items div");
+    const spanIcon = this.$wrapper.querySelector(".fa-solid");
 
-    const toggleSelectItems = () => selectItems.classList.toggle("select_hide");
-    const showSelectItems = () => selectItems.classList.remove("select_hide");
-    const hideSelectItems = () => selectItems.classList.add("select_hide");
+    selectedDiv.innerHTML = options[0].innerHTML;
+
+    const toggleSelectItems = () => {
+      selectItems.classList.toggle("select_hide");
+      selectItems.classList.toggle("open");
+      spanIcon.classList.toggle("fa-rotate-180");
+    };
+
+    const showSelectItems = () => {
+      selectItems.classList.remove("select_hide");
+      spanIcon.classList.add("fa-rotate-180");
+    };
+    const hideSelectItems = () => {
+      selectItems.classList.add("select_hide");
+      selectItems.classList.remove("open");
+      spanIcon.classList.remove("fa-rotate-180");
+    };
+
+    // Hide the div that contains the same content as the one displayed
+    const updateSelectItemsVisibility = () => {
+      const selectedContent = selectedDiv.innerHTML;
+      options.forEach((option) => {
+        if (option.innerHTML === selectedContent) {
+          option.style.display = "none";
+        } else {
+          option.style.display = "block";
+        }
+      });
+    };
+
+    function updateDividersVisibility() {
+      const dividers = selectItems.querySelectorAll(".divider");
+
+      dividers.forEach((divider) => {
+        const nextElement = divider.nextElementSibling;
+
+        if (nextElement && nextElement.tagName === "DIV") {
+          if (nextElement.style.display === "none") {
+            divider.style.display = "none";
+          } else {
+            divider.style.display = "block";
+          }
+        }
+      });
+    }
+    updateSelectItemsVisibility();
+    updateDividersVisibility();
 
     selectedDiv.addEventListener("click", toggleSelectItems);
     select.addEventListener("focus", showSelectItems);
@@ -43,26 +97,27 @@ class SortBox {
     for (let i = 0; i < options.length; i++) {
       options[i].addEventListener("click", () => {
         selectedDiv.innerHTML = options[i].innerHTML;
-        select.value = options[i].innerHTML;
         hideSelectItems();
+        updateSelectItemsVisibility();
+        updateDividersVisibility();
       });
     }
 
+    // Adds a click event listener to the document to hide the custom select items
+    // when clicking outside of the custom select element.
     document.addEventListener("click", (e) => {
-      /**
-       * Check if the clicked element is not a descendant of the custom select menu.
-       * If true, hide the select menu.
-       */
       if (!e.target.closest(".custom_select")) {
         hideSelectItems();
       }
     });
 
+    // Keyboard navigation for accessibility
     select.addEventListener("keydown", (e) => {
       if (!selectItems.classList.contains("select_hide")) {
         if (e.key === "ArrowUp" && select.selectedIndex > 0) {
           select.selectedIndex -= 1;
           selectedDiv.innerHTML = options[select.selectedIndex].innerHTML;
+          spanIcon.classList.add("fa-rotate-180");
           e.preventDefault();
         } else if (
           e.key === "ArrowDown" &&
@@ -70,13 +125,18 @@ class SortBox {
         ) {
           select.selectedIndex += 1;
           selectedDiv.innerHTML = options[select.selectedIndex].innerHTML;
+          spanIcon.classList.add("fa-rotate-180");
           e.preventDefault();
         } else if (e.key === "Enter") {
           selectedDiv.innerHTML =
             select.options[select.selectedIndex].innerHTML;
           select.value = select.options[select.selectedIndex].innerHTML;
+          spanIcon.classList.remove("fa-rotate-180");
           hideSelectItems();
         }
+
+        updateSelectItemsVisibility();
+        updateDividersVisibility();
       }
     });
   }

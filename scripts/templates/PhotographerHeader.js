f12fd368d8060784df928fbf0090801b31d8e2e3
Elishaya13 <a.lartillier@gmail.com>
2023-09-15 09:53:43 +0200

    Fix : Pixelation of images on other browsers and positioning of the contact button

--- scripts/templates/PhotographerHeader.js
+++ scripts/templates/PhotographerHeader.js
@@ -6,19 +6,21 @@ class PhotographerHeader {
     const photographerData = new Photographer(this.photographer);
 
     const photographHeader = `
-    <div class="photographer_infos_container" data-testid="photographer-infos-container">
-      <h1>${photographerData.name}</h1>
-      <h2>${photographerData.city}, ${this.photographer.country}</h2>
-      <p>${photographerData.tagline}</p>
-    </div>  
-      <button class="contact_button" aria-labelledby="contact-name" onclick="displayModal()">Contactez-moi</button>
-    <div class="photographer_profil_container" data-testid="photographer-profil-container">  
-      <img          
-            alt="${photographerData.name}"
-            src="${photographerData.portrait}"
-          
-        /> 
-    </div>          
+      <div class="photographer_infos_container" data-testid="photographer-infos-container">
+        <h1>${photographerData.name}</h1>
+        <h2>${photographerData.city}, ${this.photographer.country}</h2>
+        <p>${photographerData.tagline}</p>
+      </div>  
+      <div class="photographer_button_container">
+         <button class="contact_button" aria-labelledby="contact-name" onclick="displayModal()">Contactez-moi</button>
+      </div>   
+      <div class="photographer_profil_container" data-testid="photographer-profil-container">  
+        <img          
+              alt="${photographerData.name}"
+              src="${photographerData.portrait}"
+            
+          /> 
+      </div>          
        `;
 
     return photographHeader;
